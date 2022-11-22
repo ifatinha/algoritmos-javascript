a5969f2849440d0cd083057e4812a885946216b4
Fatima Ferreira <ifatinha99@gmail.com>
2022-11-22 15:17:38 -0300

    exercicios

--- exemplos-basicos/exemplo-24.js
+++ exemplos-basicos/exemplo-24.js
@@ -8,14 +8,14 @@ import scanner from "readline-sync";
 let numero = parseFloat(scanner.question("Digite um numero fracionario: "));
 
 let parteInteira = Math.trunc(numero);
-let parteFracionaria = numero % 2;
+let parteFracionaria = numero.toString().split(".");
 let numeroArredondado = Math.round(numero);
 let arredondadoParaCima = Math.ceil(numero);
 let arredondadoParaBaixo = Math.floor(numero);
 
 
 console.log("Parte inteira: " + parteInteira);
-console.log("Parte fracionária: "+parteFracionaria);
+console.log("Parte fracionária: " + "." + parteFracionaria[1]);
 console.log("Numero arredondado: " + numeroArredondado);
-console.log("Arredondado para cima: " + arredondadoParaCima)
-console.log("Arredondado para baixo: " + arredondadoParaBaixo)
+console.log("Arredondado para cima: " + arredondadoParaCima);
+console.log("Arredondado para baixo: " + arredondadoParaBaixo);
