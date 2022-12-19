ba744dca2cf1c4fb0aeda38ba1ccac9ef946b08d
Fatima Ferreira <ifatinha99@gmail.com>
2022-12-19 20:15:37 -0300

    exercicios arrays

--- arrays/exemplo-03.js
+++ arrays/exemplo-03.js
@@ -3,13 +3,23 @@
 
 import scanner from 'readline-sync'
 
-let numeros1 = [];
-let numeros2 = [];
+const numeros1 = [];
+const numeros2 = [];
+const numeros = [];
 
 for (let i = 0; i < 10; i++) {
     numeros1.push(Math.floor((Math.random() * 99) + 1));
+    numeros.push(numeros1[i]);
+
     numeros2.push(Math.floor((Math.random() * 99) + 1));
+    numeros.push(numeros2[i]);
 }
 
-console.log(numeros1.toString());
-console.log(numeros2.toString());
+console.log("Elementos do primeiro vetor.");
+console.log(numeros1);
+
+console.log("Elementos do segundo vetor.");
+console.log(numeros2);
+
+console.log("Intercalação dos vetores");
+console.log(numeros);
