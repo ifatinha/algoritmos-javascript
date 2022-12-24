a8496bc6665e21c556c4e0bc95c48ad49b1650c2
Fatima Ferreira <ifatinha99@gmail.com>
2022-12-24 19:29:15 -0300

    arrays

---
+++ arrays/exemplo-25.js
@@ -0,0 +1,28 @@
+/** Capitulo 06 - Vetores: Exemplo 25
+ *  Livro Fundamentos da Programação de Computadores - Algoritmos em Pascal, C e C++ */
+
+import scanner from 'readline-sync'
+
+const primos = [];
+let divisores = 0;
+let contador = 0;
+let numero = 101;
+
+while (contador < 10) {
+    
+    for (let i = 1; i <= numero; i++) {
+        if (numero % i === 0) {
+            divisores++;
+        }
+    }
+
+    if (divisores === 2) {
+        primos.push(numero);
+        contador++;
+    }
+
+    divisores = 0;
+    numero++;
+}
+
+console.log("Os 10 primeiros números primos acima de 100 são: " + primos.join(" - "));
