d2461a1f399715c35c217fcac345a91872871d66
Fatima Ferreira <ifatinha99@gmail.com>
2023-01-18 11:14:51 -0300

    exemplos funcoes

--- funcoes/exemplo-21.js
+++ funcoes/exemplo-21.js
@@ -3,18 +3,49 @@
 
 import scanner from 'readline-sync'
 
-let x, y;
+function somaPrimos(x, y) {
+    let soma = 0;
 
-do {
-    x = scanner.questionInt("Valor de X: ");
-    y = scanner.questionInt("Valor de Y > X: ");
+    while (x <= y) {
+        let qtdDivisores = 0;
+        for (let i = 1; i <= x; i++) {
+            if (x % i === 0) {
+                qtdDivisores++;
+            }
+        }
 
-    while (v <= x) {
-        y = scanner.questionInt("O valor de Y deve maior que o valor de X. Valor de Y: ");
+        if (qtdDivisores === 2) {
+            console.log(x + " é primo");
+            soma += x;
+        }
+
+        x++;
     }
 
-} while (x !== y)
-
-function primos(x, y) {
-    
+    return soma;
 }
+
+function programaPrincipal() {
+    let x, y;
+
+    do {
+        x = scanner.questionInt("Valor de X: ");
+        y = scanner.questionInt("Valor de Y > X: ");
+
+        while (y < x) {
+            y = scanner.questionInt("O valor de Y deve maior que o valor de X. Valor de Y: ");
+        }
+
+        console.clear();
+        console.log("Valor de X: " + x);
+        console.log("Valor de Y: " + y);
+        if (x === y) {
+            console.log("X e Y são iguais. Encerrando aplicação!");
+        } else {
+            console.log("Soma dos numeros primos entre X e Y: " + somaPrimos(x, y));
+        }
+
+    } while (x !== y)
+}
+
+programaPrincipal();
---
+++ funcoes/exemplo-25.js
@@ -0,0 +1,31 @@
+/** Capitulo 06 - sub-rotinas: Exemplo 25
+ *  Livro Fundamentos da Programação de Computadores - Algoritmos em Pascal, C e C++ */
+
+import scanner from 'readline-sync';
+
+function substituicao(vetor) {
+    const vet = [];
+    for (let i = 0; i < 25; i++) {
+        if (vetor[i] < 0) {
+            vet.push(0);
+        } else {
+            vet.push(vetor[i]);
+        }
+    }
+
+    return vet;
+}
+
+function principal() {
+    const numeros = [];
+
+    for (let i = 0; i < 25; i++) {
+        numeros.push(scanner.questionInt("Numero " + (i + 1) + ": "));
+    }
+
+    console.clear();
+    const novoVetor = substituicao(numeros);
+    console.log(novoVetor.join(", "));
+}
+
+principal();
