fd63d6c690ce2416e86db5d1673a82caaa9e1b62
Fatima Ferreira <ifatinha99@gmail.com>
2023-01-22 19:44:06 -0300

    funcoes

--- funcoes/exercicio-20.js
+++ funcoes/exercicio-20.js
@@ -3,3 +3,51 @@
 
 import scanner from 'readline-sync';
 
+const salarios = [];
+const filhos = [];
+
+function lerDados() {
+    let resposta;
+    let contador = 1;
+    do {
+
+        console.log("Dados do participante " + contador);
+        salarios.push(scanner.questionFloat("Salario: "));
+        filhos.push(scanner.questionInt("Numero de filhos: "));
+
+        resposta = scanner.question("Informar outro participante? ");
+        contador++;
+        console.clear();
+
+    } while (resposta.toLowerCase() === "sim")
+}
+
+function somaSalarial(total, salario) {
+    return total + salario;
+}
+
+function totalFilhos(total, qtd) {
+    return total + qtd;
+}
+
+function maiorSalario(salarios) {
+    let maior = salarios[0];
+
+    for (let i = 1; i < salarios.length; i++) {
+        if (maior < salarios[i]) {
+            maior = salarios[i];
+        }
+    }
+
+    return maior;
+}
+
+lerDados();
+let mediaSalarios = salarios.reduce(somaSalarial) / salarios.length;
+console.log("Média Salarial da população: " + mediaSalarios.toFixed(2));
+
+let mediaFilhos = filhos.reduce(totalFilhos) / filhos.length;
+console.log("Média de Filhos da população: " + mediaFilhos);
+
+let maiorSal = maiorSalario(salarios);
+console.log("Maior sálario: " + maiorSal.toFixed(2));
