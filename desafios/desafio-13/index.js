8730ac87aac8a50bc70eed568521318433ec1ece
Fatima Ferreira <ifatinha99@gmail.com>
2023-04-21 21:55:17 -0300

    desafios

--- desafios/desafio-13/index.js
+++ desafios/desafio-13/index.js
@@ -1,15 +1,12 @@
 import app from './util/App.js'
 
-const precoMinuto = 1.99;
+const dataInicio = app.lerHorario("Digite a hora de inicio da chamada: ",
+    "Digite o minuto de inicio da chamada: ");
+const dataFinal = app.lerHorario("Digite a hora de termino da chamada: ",
+    "Digite o minuto final da chamada: ");
 
-const dataInicio = app.lerHorario("Digite a hora de inicio da chamada: ");
-const dataFinal = app.lerHorario("Digite a hora de termino da chamada: ");
-
-let minutosGastos = app.calculcarMinutos(dataInicio, dataFinal);
-console.log("Duração da chamada: " + minutosGastos + "min");
-
-let totalPagar = app.calcularTotalPagar(minutosGastos, precoMinuto);
-console.log("Total a pagar: R$" + totalPagar.toFixed(2));
+console.clear();
+console.log("Horario de inicio da chamada: " + dataInicio.toLocaleTimeString());
+console.log("Horario de término da chamada: " + dataFinal.toLocaleTimeString());
 
 app.calcularDesconto(dataInicio, dataFinal);
-app.tabelaPrecos();
--- desafios/desafio-13/util/App.js
+++ desafios/desafio-13/util/App.js
@@ -1,16 +1,8 @@
 import scanner from 'readline-sync'
 
-function tabelaPrecos() {
-    console.log("Tabela de preços\n" +
-        "das 0h às 9h — 50% de desconto no valor do minuto.\n" +
-        "das 9h01 às 18h — 0% de desconto no valor do minuto.\n" +
-        "das 18h01 às 21h — 30% de desconto no valor do minuto.\n" +
-        "das 21h01 às 23h59 — 40% de desconto no valor do minuto.");
-}
-
-function lerHorario(msg) {
-    let hora = scanner.questionInt(msg);
-    let minuto = scanner.questionInt(msg);
+function lerHorario(msgHora, msgMinuto) {
+    let hora = scanner.questionInt(msgHora);
+    let minuto = scanner.questionInt(msgMinuto);
     const date = new Date();
     date.setHours(hora);
     date.setMinutes(minuto);
@@ -18,36 +10,129 @@ function lerHorario(msg) {
     return date;
 }
 
-function calculcarMinutos(dataInicio, dataFim) {
+function calcularMinutos(dataInicio, dataFim) {
     let diferenca = dataFim.getTime() - dataInicio.getTime();
     let minutos = Math.trunc((diferenca / 60000));
     return minutos;
 }
 
-function calcularTotalPagar(totalMinutos, precoMinuto) {
+function calcularTotalPagar(totalMinutos) {
+    const precoMinuto = 1.99;
     return (totalMinutos * precoMinuto);
 }
 
+function calcularValorFaixa(codFaixa, dataInicio, dataFinal) {
+    let minutosGastos = calcularMinutos(dataInicio, dataFinal);
+    let totalSemDesconto = calcularTotalPagar(minutosGastos);
+    let totalComDesconto = totalSemDesconto;
+    let msg = "";
+
+    switch (codFaixa) {
+        case 9:
+            totalComDesconto -= totalSemDesconto * 0.5;
+            msg = "\nMinutos gastos das " + dataInicio.getHours() + "h" + dataInicio.getMinutes() +
+                "min às " + dataFinal.getHours() + "h" + dataFinal.getMinutes() + "min: ";
+            break;
+
+        case 18:
+            msg = "\nMinutos gastos das " + dataInicio.getHours() + "h" + dataInicio.getMinutes() +
+                "min às " + dataFinal.getHours() + "h" + dataFinal.getMinutes() + "min: ";
+            totalComDesconto -= totalSemDesconto * 0;
+            break;
+
+        case 21:
+            msg = "\nMinutos gastos das " + dataInicio.getHours() + "h" + dataInicio.getMinutes() +
+                "min às " + dataFinal.getHours() + "h" + dataFinal.getMinutes() + "min: ";
+            totalComDesconto -= totalSemDesconto * 0.3;
+            break;
+
+        case 23:
+            msg = "\nMinutos gastos das " + dataInicio.getHours() + "h" + dataInicio.getMinutes() +
+                "min às " + dataFinal.getHours() + "h" + dataFinal.getMinutes() + "min: ";
+            totalComDesconto -= totalSemDesconto * 0.4;
+            break
+    }
+
+    console.log(msg + minutosGastos + "\n" +
+        "Total sem desconto: R$" + totalSemDesconto.toFixed(2) + "\n" +
+        "Total com desconto: R$" + totalComDesconto.toFixed(2));
+    return totalComDesconto;
+}
+
+function alterarHorarioInicial(hora, minuto) {
+    let date = new Date();
+    date.setHours(hora);
+    date.setMinutes(minuto);
+    return date;
+}
+
 function calcularDesconto(dataInicio, dataFinal) {
-    let desconto = 0;
+    let totalPagar = 0;
+    let date = new Date();
 
-    if (dataInicio.getHours() > 0 && dataFinal.getHours() <= 9) {
-        console.log("Desconto de 50%");
-    }
+    if (dataInicio.getHours() >= 0) {
 
-    if (dataInicio.getHours() > 9 && dataFinal.getHours() <= 18) {
-        console.log("Desconto de 0%");
-    }
+        if (dataFinal.getHours() <= 9) {
+            totalPagar = calcularValorFaixa(9, dataInicio, dataFinal);
+            console.log("Total pagar: R$" + totalPagar.toFixed(2) + "\n");
+        } else if (dataFinal.getHours() <= 18) {
 
-    if (dataInicio.getHours() > 18 && dataFinal.getHours() <= 21) {
-        console.log("Desconto de 30%");
-    }
+            if (dataInicio.getHours() <= 9) {
+                date = alterarHorarioInicial(9, 0);
+                totalPagar = calcularValorFaixa(9, dataInicio, date);
+                totalPagar += calcularValorFaixa(18, date, dataFinal);
+            } else {
+                totalPagar += calcularValorFaixa(18, dataInicio, dataFinal);
+            }
 
-    if (dataInicio.getHours() > 21 && dataFinal.getHours() <= 23) {
+            console.log("\nTotal pagar: R$" + totalPagar.toFixed(2));
+        } else if (dataFinal.getHours() <= 21) {
+            let dateAux;
+            if (dataInicio.getHours() <= 9) {
+                date = alterarHorarioInicial(9, 0);
+                totalPagar = calcularValorFaixa(9, dataInicio, date);
+                dateAux = alterarHorarioInicial(18, 0);
+                totalPagar += calcularValorFaixa(18, date, dateAux);
+                totalPagar += calcularValorFaixa(21, dateAux, dataFinal);
+            } else if (dataFinal.getHours() <= 18) {
+                dateAux = alterarHorarioInicial(18, 0);
+                totalPagar += calcularValorFaixa(18, dataInicio, dateAux);
+                totalPagar += calcularValorFaixa(21, dateAux, dataFinal);
+            } else {
+                totalPagar += calcularValorFaixa(21, dataInicio, dataFinal);
+            }
+
+            console.log("\nTotal pagar: R$" + totalPagar.toFixed(2));
+        } else if (dataFinal.getHours() <= 23) {
+            let dateAux;
+            if (dataInicio.getHours() <= 9) {
+                date = alterarHorarioInicial(9, 0);
+                totalPagar = calcularValorFaixa(9, dataInicio, date);
+
+                dateAux = alterarHorarioInicial(18, 0)
+                totalPagar += calcularValorFaixa(18, date, dateAux);
+
+                date = alterarHorarioInicial(21, 0);
+                totalPagar += calcularValorFaixa(21, dateAux, date);
+
+                totalPagar += calcularValorFaixa(23, date, dataFinal);
+            } else if (dataInicio.getHours() <= 18) {
+                console.log("estou aqui");
+                dateAux = alterarHorarioInicial(18, 0)
+                totalPagar += calcularValorFaixa(18, dataInicio, dateAux);
+                date = alterarHorarioInicial(21, 0);
+                totalPagar += calcularValorFaixa(21, dateAux, date);
+                totalPagar += calcularValorFaixa(23, date, dataFinal);
+            } else {
+                totalPagar += calcularValorFaixa(23, dataInicio, dataFinal);
+            }
+
+            console.log("\nTotal pagar: R$" + totalPagar.toFixed(2));
+        }
 
     }
 }
 export default {
-    tabelaPrecos, lerHorario, calculcarMinutos,
+    lerHorario,
     calcularTotalPagar, calcularDesconto
 }
